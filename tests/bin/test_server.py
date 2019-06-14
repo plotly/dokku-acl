1f7c6cf42609a62c52c78d7c3ad2d070984e9589
Chuck <chuck@plot.ly>
2019-06-13 21:24:44 -0400

    mock auth server + pem
    pem effectively never expires

---
+++ tests/bin/test_server.py
@@ -0,0 +1,26 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+import ssl
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+
+        if 'true' in self.path:
+            resp = b'{"is_admin": "true"}'
+        else:
+            resp = b'{"is_admin": "false"}'
+        self.wfile.write(resp)
+
+test_server_path = os.path.dirname(os.path.realpath(__file__))
+httpd = HTTPServer(('localhost', 4443), SimpleHTTPRequestHandler)
+
+# Uses server.pem, which effectively never expires. 
+httpd.socket = ssl.wrap_socket(
+        httpd.socket,
+        server_side=True,
+        certfile="{}/server.pem".format(test_server_path)
+)
+httpd.serve_forever()
